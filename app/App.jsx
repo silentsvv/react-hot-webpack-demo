f32383c0be35d9cb5a4ffe7277186d27c0c99376
silentsvv <wzhq@aiitec.com>
2017-11-21 21:36:09 +0800

    add: react-router

--- app/App.jsx
+++ app/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './pages/Home/Home.jsx';
+import MyServer from './pages/Server/Server.jsx';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends React.Component {
   render() {
-    return <div>tt222t</div>
+    return <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+
+        <hr/>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={Home}/>
+        <Route path="/topics" component={MyServer}/>
+      </div>
+    </Router>
   }
 }
 
---
+++ app/pages/Server/Server.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class MyServer extends React.Component {
+  render() {
+    return <div>S2222erver</div>
+  }
+}
+
+export default MyServer;
